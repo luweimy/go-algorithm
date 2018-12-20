6f534e05b7fc77271d608d23cf24f7f3d768806d
luwei5 <luwei5@xiaomi.com>
2018-12-20 13:42:42 +0800

    add stairs dp

--- stairs/main.go
+++ stairs/main.go
@@ -16,6 +16,20 @@ func stairs(n int) int {
 	return stairs(n-1) + stairs(n-2)
 }
 
+// 走台阶问题(DP)
+func stairs2(n int) int {
+	// 状态为当前台阶的走法
+	// f(n)=f(n-1)+f(n-2)
+	var dp = make([]int, n+1)
+	dp[1] = 1
+	dp[2] = 2
+	for i := 3; i <= n; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
+	}
+	return dp[n]
+}
+
 func main() {
 	fmt.Println("stairs=>", stairs(10))
+	fmt.Println("stairs2=>", stairs2(10))
 }
